dadf249ac44f4c8467978e08ce0b9065886eaf32
Brooks Nuss <bnussjr@gmail.com>
2017-09-21 02:51:45 -0400

    Disregard previous commit. False positive on the interconnectivity. Currently working on developing a signaling server with socket.io. Once that's done, client code can be written to communicate with the server.

--- src/app/chatrooms/chat.component.ts
+++ src/app/chatrooms/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { AudioContextService } from '../audiocontext.service';
 import * as io from 'socket.io-client';
 // import '@types/webaudioapi';
@@ -10,16 +10,13 @@ import * as io from 'socket.io-client';
 
 export class ChatComponent implements AfterViewInit {
 	audioContext:AudioContext;
-	text;
-	isChannelReady = false;
-	isInitiator = false;
-	isStarted = false;
 	localStream;
 	streamDest:MediaStreamAudioDestinationNode;
-	pc;
-	remoteStream;
-	incomingStream;
-	turnReady;
+	name: string; 
+	connectedUser;
+	yourConn; 
+	stream;
+	conn;
 	pcConfig = {
   	'iceServers': [{
     'urls': 'stun:stun.l.google.com:19302'
@@ -30,17 +27,9 @@ export class ChatComponent implements AfterViewInit {
 	  offerToReceiveVideo: false
 	};
 
-	room = 'foo';
-	// Could prompt for this.room name:
-	// this.room = prompt('Enter this.room name:');
-
 	socket = io.connect();
 
 	constructor(protected audioContextService: AudioContextService){}
-
-	updateText(value: string) {
-		this.text = value;
-	}
 
 	@ViewChild("output") output: ElementRef;
 	
@@ -48,298 +37,204 @@ export class ChatComponent implements AfterViewInit {
 		this.audioContext = this.audioContextService.getAudioContext();
 
 		this.streamDest = this.audioContextService.streamDest;
-		console.log(this.audioContextService.pedalArr);
 		this.localStream = this.streamDest.stream;
-
-		this.sendMessage("Stream ready");
-		if(this.isInitiator)
-			this.maybeStart();
-
-		if (this.room !== '') {
-		  this.socket.emit('create or join', this.room);
-		  console.log('Attempted to create or  join room', this.room);
-		}
-
-		this.socket.on('created', function(room) {
-		  console.log('Created this.room ' + room);
-		 this.isInitiator = true;
-		});
-
-		this.socket.on('full', function(room) {
-		  console.log('this.room ' + room + ' is full');
-		});
-
-		this.socket.on('join', function (room){
-		  console.log('Another peer made a request to join room ' + room);
-		  console.log('This peer is the initiator of room ' + room + '!');
-		  this.isChannelReady = true;
-		});
-
-		this.socket.on('joined', function(room) {
-		  console.log('joined: ' + room);
-		  this.isChannelReady = true;
-		});
-
-		this.socket.on('log', function(array) {
-		  console.log.apply(console, array);
-		});
-
-		this.socket.on('message', function(message) {
-		  console.log('Client received message:', message);
-		  if (message === 'got user media') {
-		    this.maybeStart();
-		  } else if (message.type === 'offer') {
-		    if (!this.isInitiator && !this.isStarted) {
-		      this.maybeStart();
-		    }
-		    this.pc.setRemoteDescription(new RTCSessionDescription(message));
-		    this.doAnswer();
-		  } else if (message.type === 'answer' && this.isStarted) {
-		    this.pc.setRemoteDescription(new RTCSessionDescription(message));
-		  } else if (message.type === 'candidate' && this.isStarted) {
-		    var candidate = new RTCIceCandidate({
-		      sdpMLineIndex: message.label,
-		      candidate: message.candidate
-		    });
-		    this.pc.addIceCandidate(candidate);
-		  } else if (message === 'bye' && this.isStarted) {
-		    this.handleRemoteHangup();
-		  }
-		});
+		console.log("Got local audio stream");
+	}
+ 
+ 	start() {
+	//connecting to our signaling server 
+	this.conn = new WebSocket('ws://localhost:9090');
+	 
+	this.conn.onopen = function () { 
+	   console.log("Connected to the signaling server"); 
+	};
+	 
+	//when we got a message from a signaling server 
+	this.conn.onmessage = function (msg) { 
+	   console.log("Got message", msg.data); 
+	   var data = JSON.parse(msg.data); 
 		
+	   switch(data.type) { 
+	      case "login": 
+	         this.handleLogin(data.success); 
+	         break; 
+	      //when somebody wants to call us 
+	      case "offer": 
+	         this.handleOffer(data.offer, data.name); 
+	         break; 
+	      case "answer": 
+	         this.handleAnswer(data.answer); 
+	         break; 
+	      //when a remote peer sends an ice candidate to us 
+	      case "candidate": 
+	         this.handleCandidate(data.candidate); 
+	         break; 
+	      case "leave": 
+	         this.handleLeave(); 
+	         break; 
+	      default: 
+	         break; 
+	   } 
+	}; 
+
+		this.conn.onerror = function (err) { 
+		   console.log("Got error", err); 
+		};
 	}
-	////////////////////////////////////////////////
+	 
+	//alias for sending JSON encoded messages 
+	send(message) { 
+	   //attach the other peer username to our messages 
+	   if (this.connectedUser) { 
+	      message.name = this.connectedUser; 
+	   } 
+		
+	   this.conn.send(JSON.stringify(message)); 
+	};
+	 
+	//****** 
+	//UI selectors block 
+	//****** 
 
-	sendMessage(message) {
-	  console.log('Client sending message: ', message);
-	  this.socket.emit('message', message);
-	}
+	// var loginPage = document.querySelector('#loginPage'); 
+	// var usernameInput = document.querySelector('#usernameInput'); 
+	// var loginBtn = document.querySelector('#loginBtn');
 
-	// This client receives a message
-	
+	// var callPage = document.querySelector('#callPage'); 
+	// var callToUsernameInput = document.querySelector('#callToUsernameInput');
+	// var callBtn = document.querySelector('#callBtn'); 
 
-	////////////////////////////////////////////////////
+	// var hangUpBtn = document.querySelector('#hangUpBtn'); 
+	// var localAudio = document.querySelector('#localAudio'); 
+	// var remoteAudio = document.querySelector('#remoteAudio'); 
 
-	// if (location.hostname !== 'localhost') {
-	//   requestTurn(
-	//     'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
-	//   );
-	// }
 
-	maybeStart() {
-	  console.log('>>>>>>> maybeStart() ', this.isStarted, this.localStream, this.isChannelReady);
-	  if (!this.isStarted && typeof this.localStream !== 'undefined' && this.isChannelReady) {
-	    console.log('>>>>>> creating peer connection');
-	    this.createPeerConnection();
-	    this.pc.addStream(this.localStream);
-	    this.isStarted = true;
-	    console.log('isInitiator',this.isInitiator);
-	    if (this.isInitiator) {
-	      this.doCall();
-	    }
-	  }
-	}
+	// callPage.style.display = "none";
+	 
+	// Login when the user clicks the button 
+	login() { 
+	   this.name = "user"; 
+		
+	   if (this.name.length > 0) { 
+	      this.send({ 
+	         type: "login", 
+	         name: name 
+	      }); 
+	   } 
+		
+	};
+	 
+	handleLogin(success) { 
+	   if (success === false) { 
+	      alert("Ooops...try a different username"); 
+	   } else { 
+	      // loginPage.style.display = "none"; 
+	      // callPage.style.display = "block"; 
+			
+	      //********************** 
+	      //Starting a peer connection 
+	      //********************** 
+			
+	      //getting local audio stream 
+        this.stream = this.streamDest.stream; 
+		
+        //using Google public stun server 
+        var configuration = { 
+           "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }] 
+        }; 
+		
+        this.yourConn = new webkitRTCPeerConnection(configuration); 
+		
+        // setup stream listening 
+        this.yourConn.addStream(this.stream); 
+		
+        //when a remote user adds stream to the peer connection, we display it 
+        this.yourConn.onaddstream = incStream => { 
+           // remoteAudio.src = window.URL.createObjectURL(e.stream);
+           this.incStreamNode = this.audioContext
+           	.createMediaStreamSource(incStream.stream);
 
-	// window.onbeforeunload = function() {
-	//   sendMessage('bye');
-	// };
-
-	/////////////////////////////////////////////////////////
-
-	createPeerConnection() {
-	  try {
-	    this.pc = new RTCPeerConnection(null);
-	    this.pc.onicecandidate = this.handleIceCandidate;
-	    this.pc.onaddstream = this.handleRemoteStreamAdded;
-	    this.pc.onremovestream = this.handleRemoteStreamRemoved;
-	    console.log('Created RTCPeerConnnection');
-	  } catch (e) {
-	    console.log('Failed to create PeerConnection, exception: ' + e.message);
-	    alert('Cannot create RTCPeerConnection object.');
-	    return;
-	  }
-	}
-
-	handleIceCandidate(event) {
-	  console.log('icecandidate event: ', event);
-	  if (event.candidate) {
-	    this.sendMessage({
-	      type: 'candidate',
-	      label: event.candidate.sdpMLineIndex,
-	      id: event.candidate.sdpMid,
-	      candidate: event.candidate.candidate
-	    });
-	  } else {
-	    console.log('End of candidates.');
-	  }
-	}
-
-	handleRemoteStreamAdded(event) {
-	  console.log('Remote stream added.');
-	  this.remoteStream = event.stream;
-	  this.incomingStream = this.audioContext
-	  	.createMediaStreamSource(this.remoteStream);
-	  this.incomingStream.connect(this.audioContext.destination);
-	}
-
-	handleCreateOfferError(event) {
-	  console.log('createOffer() error: ', event);
-	}
-
-	doCall() {
-	  console.log('Sending offer to peer');
-	  this.pc.createOffer(this.setLocalAndSendMessage, this.handleCreateOfferError);
-	}
-
-	doAnswer() {
-	  console.log('Sending answer to peer.');
-	  this.pc.createAnswer().then(
-	    this.setLocalAndSendMessage,
-	    this.onCreateSessionDescriptionError
-	  );
-	}
-
-	setLocalAndSendMessage(sessionDescription) {
-	  // Set Opus as the preferred codec in SDP if Opus is present.
-	  //  sessionDescription.sdp = preferOpus(sessionDescription.sdp);
-	  this.pc.setLocalDescription(sessionDescription);
-	  console.log('setLocalAndSendMessage sending message', sessionDescription);
-	  this.sendMessage(sessionDescription);
-	}
-
-	onCreateSessionDescriptionError(error) {
-	  console.log('Failed to create session description: ' + error.toString());
-	}
-
-	// function requestTurn(turnURL) {
-	//   var turnExists = false;
-	//   for (var i in pcConfig.iceServers) {
-	//     if (pcConfig.iceServers[i].url.substr(0, 5) === 'turn:') {
-	//       turnExists = true;
-	//       turnReady = true;
-	//       break;
-	//     }
-	//   }
-	//   if (!turnExists) {
-	//     console.log('Getting TURN server from ', turnURL);
-	//     // No TURN server. Get one from computeengineondemand.appspot.com:
-	//     var xhr = new XMLHttpRequest();
-	//     xhr.onreadystatechange = function() {
-	//       if (xhr.readyState === 4 && xhr.status === 200) {
-	//         var turnServer = JSON.parse(xhr.responseText);
-	//         console.log('Got TURN server: ', turnServer);
-	//         pcConfig.iceServers.push({
-	//           'url': 'turn:' + turnServer.username + '@' + turnServer.turn,
-	//           'credential': turnServer.password
-	//         });
-	//         turnReady = true;
-	//       }
-	//     };
-	//     xhr.open('GET', turnURL, true);
-	//     xhr.send();
-	//   }
-	// }
-
-	handleRemoteStreamRemoved(event) {
-	  console.log('Remote stream removed. Event: ', event);
-	}
-
-	hangup() {
-	  console.log('Hanging up.');
-	  stop();
-	  this.sendMessage('bye');
-	}
-
-	handleRemoteHangup() {
-	  console.log('Session terminated.');
-	  stop();
-	 this.isInitiator = false;
-	}
-
-	stop() {
-	  this.isStarted = false;
-	  // isAudioMuted = false;
-	  // isVideoMuted = false;
-	  this.pc.close();
-	  this.pc = null;
-	}
-
-	///////////////////////////////////////////
-
-	// Set Opus as the default audio codec if it's present.
-	preferOpus(sdp) {
-	  var sdpLines = sdp.split('\r\n');
-	  var mLineIndex;
-	  // Search for m line.
-	  for (var i = 0; i < sdpLines.length; i++) {
-	    if (sdpLines[i].search('m=audio') !== -1) {
-	      mLineIndex = i;
-	      break;
-	    }
-	  }
-	  if (mLineIndex === null) {
-	    return sdp;
-	  }
-
-	  // If Opus is available, set it as the default in m line.
-	  for (i = 0; i < sdpLines.length; i++) {
-	    if (sdpLines[i].search('opus/48000') !== -1) {
-	      var opusPayload = this.extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
-	      if (opusPayload) {
-	        sdpLines[mLineIndex] = this.setDefaultCodec(sdpLines[mLineIndex],
-	          opusPayload);
-	      }
-	      break;
-	    }
-	  }
-
-	  // Remove CN in m line and sdp.
-	  sdpLines = this.removeCN(sdpLines, mLineIndex);
-
-	  sdp = sdpLines.join('\r\n');
-	  return sdp;
-	}
-
-	extractSdp(sdpLine, pattern) {
-	  var result = sdpLine.match(pattern);
-	  return result && result.length === 2 ? result[1] : null;
-	}
-
-	// Set the selected codec to the first in m line.
-	setDefaultCodec(mLine, payload) {
-	  var elements = mLine.split(' ');
-	  var newLine = [];
-	  var index = 0;
-	  for (var i = 0; i < elements.length; i++) {
-	    if (index === 3) { // Format of media starts from the fourth.
-	      newLine[index++] = payload; // Put target payload to the first.
-	    }
-	    if (elements[i] !== payload) {
-	      newLine[index++] = elements[i];
-	    }
-	  }
-	  return newLine.join(' ');
-	}
-
-	// Strip CN from sdp before CN constraints is ready.
-	removeCN(sdpLines, mLineIndex) {
-	  var mLineElements = sdpLines[mLineIndex].split(' ');
-	  // Scan from end for the convenience of removing an item.
-	  for (var i = sdpLines.length - 1; i >= 0; i--) {
-	    var payload = this.extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
-	    if (payload) {
-	      var cnPos = mLineElements.indexOf(payload);
-	      if (cnPos !== -1) {
-	        // Remove CN payload from m line.
-	        mLineElements.splice(cnPos, 1);
-	      }
-	      // Remove CN line in sdp
-	      sdpLines.splice(i, 1);
-	    }
-	  }
-
-	  sdpLines[mLineIndex] = mLineElements.join(' ');
-	  return sdpLines;
-	}
+           //create source
+        }; 
+		
+        // Setup ice handling 
+        this.yourConn.onicecandidate = event => { 
+           if (event.candidate) { 
+              this.send({ 
+                 type: "candidate", 
+                 candidate: event.candidate 
+              }); 
+           } 
+        }; 
+	   } 
+	};
+	 
+	//initiating a call 
+	call() { 
+	   var callToUsername = callToUsernameInput.value; 
+		
+	   if (callToUsername.length > 0) { 
+	      connectedUser = callToUsername; 
+			
+	      // create an offer 
+	      yourConn.createOffer(function (offer) { 
+	         send({
+	            type: "offer", 
+	            offer: offer 
+	         }); 
+				
+	         yourConn.setLocalDescription(offer); 
+	      }, function (error) { 
+	         alert("Error when creating an offer"); 
+	      }); 
+	   } 
+	};
+	 
+	//when somebody sends us an offer 
+	handleOffer(offer, name) { 
+	   this.connectedUser = name; 
+	   this.yourConn.setRemoteDescription(new RTCSessionDescription(offer)); 
+		
+	   //create an answer to an offer 
+	   this.yourConn.createAnswer(function (answer) { 
+	      this.yourConn.setLocalDescription(answer); 
+			
+	      this.send({ 
+	         type: "answer", 
+	         answer: answer 
+	      });
+			
+	   }, function (error) { 
+	      alert("Error when creating an answer"); 
+	   }); 
+		
+	};
+	 
+	//when we got an answer from a remote user 
+	handleAnswer(answer) { 
+	   this.yourConn.setRemoteDescription(new RTCSessionDescription(answer)); 
+	};
+	 
+	//when we got an ice candidate from a remote user 
+	handleCandidate(candidate) { 
+	   this.yourConn.addIceCandidate(new RTCIceCandidate(candidate)); 
+	};
+	 
+	//hang up
+	hangup() { 
+	   this.send({ 
+	      type: "leave" 
+	   }); 
+		
+	   this.handleLeave(); 
+	};
+	 
+	handleLeave() { 
+	   this.connectedUser = null; 
+		
+	   this.yourConn.close(); 
+	   this.yourConn.onicecandidate = null; 
+	   this.yourConn.onaddstream = null; 
+	};
 
 }
